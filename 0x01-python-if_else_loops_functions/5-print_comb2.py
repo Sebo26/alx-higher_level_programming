a9ce1804770a6f4339d1e58f763e1e1c4147a072
root <sebomaduna26@gmail.com>
2023-11-28 17:35:25 -0800

    No 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for numbers in range(00, 100):
-    print("{}, ".format(numbers))
+for numbers in range(100):
+    print("{:02d}".format(numbers), end=", " if numbers < 99 else "")
