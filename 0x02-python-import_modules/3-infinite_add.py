9e4142f71e6856f6a00a9a5b4fc86378966397ae
root <sebomaduna26@gmail.com>
2023-11-30 14:01:30 -0800

    No 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def add_arg(argv):
-    arg = sys.argv
     length = len(arg) - 1
-    add = 0
     if length == 0:
         print("{}".format(length))
+        return
     else:
         i = 1
+        add = 0
         while i <= length:
             add = add + int(argv[i])
             i = i + 1
