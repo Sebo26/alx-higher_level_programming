611f208377fe9f2ed5a93c4881d665d4fc4017e1
root <sebomaduna26@gmail.com>
2023-11-30 13:51:44 -0800

    No 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,3 +3,12 @@ if __name__ == "__main__":
     import sys
     arg = sys.argv
     length = len(arg) - 1
+    add = 0
+    if length == 0:
+        print("{}".format(length))
+    else:
+        i = 1
+        while i <= length:
+            add = add + int(argv[i])
+            i = i + 1
+        print("{}".format(add))
