6a35ccd5998dfa5ac72621bbc70b6e1e9e4cab5e
root <sebomaduna26@gmail.com>
2023-11-30 14:29:57 -0800

    No 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name ==  "__main__":
-    from variable_load_5 import a
-    print("{}".format(a))
+    find_var()
+
+from variable_load_5 import a
+def find_var():
+    print(a)
