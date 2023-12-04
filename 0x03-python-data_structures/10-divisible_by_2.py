9e19857842bfd89665cff3f262d5937acdc5c6ae
root <sebomaduna26@gmail.com>
2023-12-04 12:35:09 -0800

    No 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,7 @@ def divisible_by_2(my_list=[]):
     for i in my_list:
         modulos = my_list[i] % 2
         if modulos == 0:
+            print(i)
             return True
         else:
             return False
