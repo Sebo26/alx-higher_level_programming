d490a37a3e63f1091aff520060f24d413c4c3a0d
root <sebomaduna26@gmail.com>
2023-12-04 02:02:39 -0800

    No 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     length = len(my_list)
     if idx < 0 or idx >= length:
-        return "None"
+        return None
     else:
         print("Element at index {} is {}".format(idx, my_list[idx]))
