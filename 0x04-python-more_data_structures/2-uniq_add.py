6c0540a42800e1995b0310a17919eb2e937ce367
root <sebomaduna26@gmail.com>
2023-12-06 05:51:09 -0800

    No 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_set = set(my_list)
-    result = sum(unique_set)
-    print(result)
+    def summation(a, b):
+        return a + b
+    print(list(map(summation, unique_set)))
