4f2565887f22f1a413b22e602b9dc3ae7773498d
root <sebomaduna26@gmail.com>
2023-12-06 02:49:59 -0800

    No 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
-    result = []
-    for row in matrix:
-        new_row = [elem ** 2 for elem in row]
-        result.append(new_row)
-    print(result)
+    map(sqr_root, matrix)
+    def sqr_root(num):
+        return num ** num
