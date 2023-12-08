7a37c181b890a7d2a2035b2e58da2d39da64c031
root <sebomaduna26@gmail.com>
2023-12-08 15:46:10 -0800

    No 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    r = list(map(lambda(set_1, set_2: set_1 + set_2)))
-    common = {set_1 + set_2}
+    common = set_1.intersection(set_2)
     return (common)
