81a1c338108a52ac35457dac50a2e9aa782ea472
root <sebomaduna26@gmail.com>
2023-12-09 06:18:59 -0800

    No 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for keys in a_dictionary:
-        print(sorted(keys))
+    for keys in sorted(a_dictionary):
+        print(keys)
