afceedc8fdf55ddaec9de2eb553e00aacbc91def
root <sebomaduna26@gmail.com>
2023-12-09 15:43:17 -0800

    No 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for values in a_dictionary:
-        values = a_dictionary.values() * 2
-    return (values)
+    for key in a_dictionary:
+        a_dictionary[key] *= 2
+    return (a_dictionary)
