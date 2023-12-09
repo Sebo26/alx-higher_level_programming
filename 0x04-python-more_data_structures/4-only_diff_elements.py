8895119e1a748a50494c59c62fa4497da23ceb48
root <sebomaduna26@gmail.com>
2023-12-09 05:44:05 -0800

    No 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    diff_1 = set_1 - set_2
-    diff_2 = set_2 - set_1
-    result = diff_1.union(diff_2)
-    print(result)
+    together = set_1 + set_2
+    return (set(together))
