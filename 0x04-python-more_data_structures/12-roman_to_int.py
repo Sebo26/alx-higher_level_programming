288d4f9bf55c6fbc1ff5bc16a5db265910762f7d
root <sebomaduna26@gmail.com>
2023-12-09 16:46:49 -0800

    No 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return (0)
+    if M == 1 000:
+        add if D == 500:
+            add if C == 100:
+                add if L == 50:
+                    add if X == 10:
+                        add if V == 5:
+                            if I, II, III, IV is range(0, 5)
