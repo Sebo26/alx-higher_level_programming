2c9d3fda58516a19e38c3c0b16eef9db331bcc7c
root <sebomaduna26@gmail.com>
2023-12-18 14:08:00 -0800

    No 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-class NotDigit(Exception):
-    pass
-
+count = 0
 def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
-            if not my_list[i].isnumeric():
-                raise NotDigit
-            print("{:d}".format(int(my_list[i])), end=" ")
-    except (ValueError, NotDigit):
-            print("Not an integer", end=" ")
+            value = my_list[i]
+            if isinstance(value, int):
+                print("{:d}".format(value), end=" ")
+                count += 1
+    except IndexError:
+        pass
     print()
+    return count
                 
