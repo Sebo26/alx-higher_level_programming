04edec28509a4597df80da6106fe1a8b70d1bde7
root <sebomaduna26@gmail.com>
2023-12-19 13:57:38 -0800

    No 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Define class named Square"""
+
+
+class Square:
+    """The square class"""
+    pass
+    def __init__(self, size=0):
+        """initializing new square with attribute size"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return int(self.__size) * int(self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        if size = 0:
+            print(\n)
+        else:
+            while(size):
+                print("#")
+                while(size):
+                    print("#")
