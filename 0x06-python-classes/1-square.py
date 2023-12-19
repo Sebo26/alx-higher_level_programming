fb7ee38480ebf73c1c2951411d7000b32401f667
root <sebomaduna26@gmail.com>
2023-12-19 03:41:34 -0800

    No 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define class named Square"""
+
+
+class Square:
+    """The square class"""
+    pass
+    def __init__(self, size):
+        """initializing new square with attribute size"""
+        self.__size = size
