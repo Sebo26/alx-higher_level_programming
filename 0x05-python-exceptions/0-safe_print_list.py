d761d6017e1a16db5fd45e9bcf94691074c3ed74
root <sebomaduna26@gmail.com>
2023-12-20 05:02:50 -0800

    No 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for numbers in range(x):
-            print("{}".format(my_list[numbers], end=" ")
+            print("{}".format(my_list[numbers], end=" "))
             count = count + 1
     except IndexError:
         print("Index doesn't exist")
