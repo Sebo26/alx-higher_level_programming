4994898232d0af1ab162ca87f4a11eb6ede3fb61
root <sebomaduna26@gmail.com>
2023-12-20 06:27:15 -0800

    No 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 count = 0
 def safe_print_list_integers(my_list=[], x=0):
+    global count
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
