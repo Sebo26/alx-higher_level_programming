c963a551185f3a04a95459d2ecc255a6af8f5242
root <sebomaduna26@gmail.com>
2024-01-04 16:08:12 -0800

    No 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,10 +14,12 @@ def __init__(self, width=0, height=0):
 
     @property
     def width(self):
+    """getting the width of the rectangle"""
         return int(self.__width)
 
     @width.setter
     def width(self, value):
+    """setting the width of the triangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -27,10 +29,12 @@ def __init__(self, width=0, height=0):
 
     @property
     def height(self):
+    """getting the height of the rectangle"""
         return int(self.__length)
 
     @height.setter
     def height(self, value):
+    """Setting the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
