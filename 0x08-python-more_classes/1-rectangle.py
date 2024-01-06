ad8e2dc28ae13fa13b3dea8b8189d25a3bd842fa
root <sebomaduna26@gmail.com>
2024-01-06 13:57:03 -0800

    No

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ def __init__(self, width=0, height=0):
 
     @property
     def width(self):
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -33,7 +33,7 @@ def __init__(self, width=0, height=0):
 
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
