d01dd002cb42b85cfd0aa7a8c8b7edc6a74dae03
root <sebomaduna26@gmail.com>
2024-01-08 11:39:04 -0800

    No 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
