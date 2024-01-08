853873518b57dbdd5040cf9c93bd325fad652dbb
root <sebomaduna26@gmail.com>
2024-01-08 11:28:17 -0800

    No 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The function checks if two classes are the same"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if a class is an instance of another"""
+    if isinstance(obj, a_class)
+        return True
+    else:
+        return False
