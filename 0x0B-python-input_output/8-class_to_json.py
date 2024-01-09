3c65aeedecaed6f3cce330fa1bb7c5c1411a66e1
root <sebomaduna26@gmail.com>
2024-01-09 10:29:54 -0800

    No 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,8 +2,7 @@
 """Function returns the dictionary description with simple data structure"""
 
 
-import json
-
-
 def class_to_json(obj):
+    """returns the dictionary description with simple data structure"""
 
+    
