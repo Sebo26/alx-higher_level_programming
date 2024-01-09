deca2eb4dd405abd71db211613f4ed9bdbc41d9c
root <sebomaduna26@gmail.com>
2024-01-09 06:14:47 -0800

    No 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function appends string at end of file"""
+
+
+def append_write(filename="", text=""):
+    """Appends string to file and return number of characters"""
+    with open(filename, mode="a", encoding="utf-8") as myFile:
+        return (myFile.append(text))
