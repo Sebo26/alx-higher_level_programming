da123f626123faf973abe1fec303ec8c6c88e06c
root <sebomaduna26@gmail.com>
2024-01-09 05:43:58 -0800

    README and No 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function reads file and prints it in the stdout"""
+import os
+
+def read_file(filename=""):
+    """Definition for file to be read"""
+    with open(filename, encoding="utf-8) as myFile:
+    print(myFile.read())
