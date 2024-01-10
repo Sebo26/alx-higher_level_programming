f02c50dfb57f80ff0232d0ce2ae7d234671f561a
root <sebomaduna26@gmail.com>
2024-01-10 10:00:12 -0800

    No 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""The function prints the first and last name inside a string"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Prints first name and last name inside a string"""
     if not isinstance(first_name, str) or not isinstance(last_name, str):
         raise TypeError("first_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
