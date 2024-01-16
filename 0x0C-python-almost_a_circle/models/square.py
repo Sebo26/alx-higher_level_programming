606bf59f6031732b37c17a78455021f072808a23
root <you@example.com>
2024-01-15 16:39:34 -0800

    No 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Makes class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle)
+    """Create class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Create attributes of the square"""
+        super().__init__(id, x, y, size, size)
+
+    def __str__(self):
+        """Return a string stating values of square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
