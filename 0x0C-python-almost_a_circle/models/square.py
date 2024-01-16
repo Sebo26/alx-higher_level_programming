06a15cd17f3970e5ff800549d7a1bac4f07030b2
root <you@example.com>
2024-01-15 16:56:26 -0800

    No 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,34 @@ class Square(Rectangle):
         """Create attributes of the square"""
         super().__init__(id, x, y, size, size)
 
+    @property
+    def width(self):
+        """Return getter and sets properties for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Return getter and sets properties for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
     def __str__(self):
         """Return a string stating values of square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
