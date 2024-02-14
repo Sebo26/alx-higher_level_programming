175307d5d3061de6b6a543dbe21dc42c80f01ba9
root <sebomaduna26@gmail.com>
2024-02-14 15:06:33 -0800

    No 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
--- Lists all shows contained in hbtn_0d_tvshows
+-- Lists all shows contained in the database hbtn_0d_tvshows.
 SELECT title FROM tv_shows NATURAL JOIN tv_show_genres WHERE genre_id ORDER BY id ASC;
