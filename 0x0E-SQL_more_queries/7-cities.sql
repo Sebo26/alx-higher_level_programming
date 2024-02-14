2d6893769c36dc97e6ab7cfc3e07d414ea5b6490
root <sebomaduna26@gmail.com>
2024-02-14 14:20:46 -0800

    No 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@ CREATE DATABASE hbtn_0d_usa;
 -- Create table called cities.
 CREATE TABLE cities (
         id INT UNIQUE TEXT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT TEXT NOT NULL FOREIGN KEY REFERENCES states(id)
+	state_id INT TEXT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id)
         name VARCHAR(256) TEXT NOT NULL);
