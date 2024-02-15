73208d163c8c040f5c8449399521c4be78f4348c
root <sebomaduna26@gmail.com>
2024-02-15 13:26:55 -0800

    No 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
 -- Creates the database hbtn_0d_2
-CREATE DATABASE `hbtn_0d_2`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 -- Creates user user_0d_2 and password
-SELECT CREATE USER user_0d_2@localhost IDENTIFIED BY `user_0d_2_pwd`;
+SELECT CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY `user_0d_2_pwd`;
 -- Grant a select
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
