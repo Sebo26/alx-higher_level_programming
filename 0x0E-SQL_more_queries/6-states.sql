f874209468c0e289bc1a0a93dba2482733819f31
root <sebomaduna26@gmail.com>
2024-02-15 13:39:54 -0800

    No 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,7 @@
 --Create a database.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table called states.
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
