198ffb47fefdf8bbc01751cf9fa766d98671ec9d
root <sebomaduna26@gmail.com>
2024-02-15 13:34:46 -0800

    No 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table states.
 --Create a database.
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table called states.
-CREATE TABLE states (
-	id INT UNIQUE TEXT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(256) TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL);
