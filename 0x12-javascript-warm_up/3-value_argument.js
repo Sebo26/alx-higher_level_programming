6eb9db274d1a81a5d8d0b13ff1e739ecab7cf223
Sebo26 <sebomaduna26@gmail.com>
2024-03-11 17:15:12 -0700

    No 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv === NULL) {
-	console.log("No argument");
-	} else {
-		console.log(process.argv[1];
-	}
+if (process.argv[2] === null) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[3];
+}
