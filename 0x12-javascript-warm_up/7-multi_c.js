0c96eef5d163ddc594e58a1b78ad3bb266f46706
Sebo26 <sebomaduna26@gmail.com>
2024-03-11 18:00:47 -0700

    No 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let i = process.argv[2];
+let j;
+if (i === undefined) {
+  console.log('Missing number of occurrences');
+} else if (i <= 0) {
+  return;
+} else {
+  for (j = 0; j < 1; j++) {
+  console.log('C is fun');
+}
+}
