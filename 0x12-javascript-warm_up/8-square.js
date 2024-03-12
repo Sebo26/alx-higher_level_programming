d75d4f6611c0a85705c7ff7076b1bc53e5b199d0
Sebo26 <sebomaduna26@gmail.com>
2024-03-11 18:12:23 -0700

    No 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+let i = process.argv[2];
+let j;
+let k;
+if (parseInt(process.argv[2] === undefined) {
+  console.log(Missing size);
+} else {
+  for (j = 0; j < i; j++) {
+    for (k = 0; k < j; k++) {
+	  console.log('X')
+	}
+  }
+}
