cca90585c9894c5c199fe6497e33e20a92181f57
Sebo26 <sebomaduna26@gmail.com>
2024-03-11 17:26:27 -0700

    No 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv !== undefined) {
-	console.log("My number: " + parseInt(process.log[1]));
-	} else {
-	console.log("Not a number");
-	}
+if (process.argv[2] !== undefined && !isNaN(parseInt(process.argv[2]))) {
+  console.log('My number: ' + parseInt(process.log[2]));
+} else {
+  console.log('Not a number');
+}
