1503d32f894a1d65111615c5b5971e2fabd2d27d
Sebo26 <sebomaduna26@gmail.com>
2024-03-12 09:41:34 -0700

    No 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate() {
+    const temp;
+    this.width = temp;
+    this.height = this.width;
+    this.width = this.height;
+  }
+
+  double() {
+    this.width = 2(w);
+    this.height = 2(h);
+  }
+}
+
+module.exports = Rectangle;
