62ae80faf2b440f0a1bc6a8a0990c135cc765464
Sebo26 <sebomaduna26@gmail.com>
2024-03-12 08:54:29 -0700

    No 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-      if (w <= 0 || h <= 0) {
-        let emptyObj = {};
-      }
+    if (w <= 0 || h <= 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
