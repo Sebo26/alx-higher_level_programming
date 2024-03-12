2cd1062dbd1acca1dfe71e3ec1cd7b937e9cc93a
Sebo26 <sebomaduna26@gmail.com>
2024-03-12 13:13:28 -0700

    No 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+const BaseSquare = require('./5-Square');
+
+class Square extends BaseSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
