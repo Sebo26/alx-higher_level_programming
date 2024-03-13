51523791d6bb9412285f34f74ad43a230f9f2986
Sebo26 <sebomaduna26@gmail.com>
2024-03-13 11:59:22 -0700

    No 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const c;
-function add(a, b) {
+function add (a, b) {
   if (!isNaN(parseInt(a)) && !isNaN(parseInt(b))) {
-    c = parseInt(a) + parseInt(b);
+    const c = parseInt(a) + parseInt(b);
     console.log(c);
   } else {
-  console.log('NaN')
+    console.log('NaN');
   }
 }
