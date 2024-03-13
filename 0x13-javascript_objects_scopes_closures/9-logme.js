6aafd8cb44ffdd92f6c5cba90ab62f3bb0dc25a7
Sebo26 <sebomaduna26@gmail.com>
2024-03-13 14:13:11 -0700

    No 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 exports.logMe = function (item) {
-  let count = 0;
   for (let i = 0; i < item.length; i++) {
-    console.log(count + ': ' + item[i]);
-    count++;
+    console.log(i + ': ' + item[i]);
   }
 };
