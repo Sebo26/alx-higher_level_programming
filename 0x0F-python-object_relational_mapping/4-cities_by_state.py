1a80638a1738448e9847fb26331b37ebfb981f46
root <sebomaduna26@gmail.com>
2024-03-16 13:30:09 -0700

    No 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Uses database hbtn_0e_4_usa to list cities"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id")
+    rows = cur.fetchall()
+    for x in rows:
+        print(x)
+    cur.close()
+    db.close()
