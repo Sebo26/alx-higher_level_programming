cbe1e371212b1823b70a3fa53611c066a3f94d56
root <sebomaduna26@gmail.com>
2024-03-27 14:44:08 -0700

    No 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # sends delete request and displays response
-URL="https://example.com"
-content=$(curl -s -L -X DELETE "$URL")
-echo "$content" > fetched_content.txt
-rm fetched_content.txt
-echo "I'm a DELETE request"
+curl -s -L -X DELETE "$URL"
