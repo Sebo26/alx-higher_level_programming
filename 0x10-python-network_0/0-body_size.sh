e22cb560ec4e4169d22ae42ecea66a6e14ff79d9
root <sebomaduna26@gmail.com>
2024-03-27 14:57:18 -0700

    No 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # displays the size of the body of the response
-URL="https://example.com"
-content=$(curl -s -L "$URL")
-content_length=$(echo -n "$content" | wc -c)
+curl -s -L "$URL" | wc -c
