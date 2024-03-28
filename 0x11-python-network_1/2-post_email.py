d0747aa2dc841f88fde32f905a533ea738c9fb07
root <sebomaduna26@gmail.com>
2024-03-28 12:32:51 -0700

    No 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+
+URL = None
+Email = None
+
+data = urllib.parse.urlencode({'email': Email}).encode('utf-8')
+req = urllib.request.Request(URL, data=data)
+
+with urllib.request.urlopen(req) as response:
+    print("Your email is:", response.read().decode('utf-8'))
