5c9d99550b3af2fddc5e57376e7e993bbf2c5e0b
root <sebomaduna26@gmail.com>
2024-03-28 12:44:15 -0700

    No 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,3 +3,6 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     print("Body response:", response.read().decode('utf-8'))
+
+if __name__ == "__main__":
+    main()
