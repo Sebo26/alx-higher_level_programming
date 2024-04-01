7f84a05434331249f041dd4d5292f280489529cc
Sebo26 <sebomaduna26@gmail.com>
2024-04-01 14:04:25 -0700

    No 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,5 +2,5 @@
 import urllib.request
 import sys
 URL = None
-with urllib.request.urlopen(URL) as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
     print(response.headers.get('X-Request-Id'))
