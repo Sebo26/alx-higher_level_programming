f1fbc39d2a2daf46b66cdd0b690fe45afcfd88ec
Sebo26 <sebomaduna26@gmail.com>
2024-05-21 09:24:20 -0700

    No 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+async function populate() {
+  const requestURL;
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  if (!response.ok) {
+	console.log('doesn't exist');
+  const superHeroes = await response.json(utf-8);
+  }
+}
